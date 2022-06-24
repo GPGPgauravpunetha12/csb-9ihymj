a3260a7aae1984e6e315bdcab9be98d93c173b3e
Code Sandbox <hello@codesandbox.io>
2022-06-24 13:03:40 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.1aae72f053d136c4de9a984af6dcf837.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f7fbdca8327f2d69ea980697d6688f1",
+    "url": "https://gpgpgauravpunetha12.github.io/csb-9ihymj/index.html"
+  },
+  {
+    "revision": "a0e315091da104963aeb",
+    "url": "https://gpgpgauravpunetha12.github.io/csb-9ihymj/static/css/2.7ba6b55a.chunk.css"
+  },
+  {
+    "revision": "26f64c3b907d6f11a4ac",
+    "url": "https://gpgpgauravpunetha12.github.io/csb-9ihymj/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "a0e315091da104963aeb",
+    "url": "https://gpgpgauravpunetha12.github.io/csb-9ihymj/static/js/2.20433f67.chunk.js"
+  },
+  {
+    "revision": "1f547fe83214e659d3b10448756e064d",
+    "url": "https://gpgpgauravpunetha12.github.io/csb-9ihymj/static/js/2.20433f67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26f64c3b907d6f11a4ac",
+    "url": "https://gpgpgauravpunetha12.github.io/csb-9ihymj/static/js/main.abcf533f.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "https://gpgpgauravpunetha12.github.io/csb-9ihymj/static/js/main.abcf533f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11711327fea3f279903f",
+    "url": "https://gpgpgauravpunetha12.github.io/csb-9ihymj/static/js/runtime-main.c2900853.js"
+  }
+]);
